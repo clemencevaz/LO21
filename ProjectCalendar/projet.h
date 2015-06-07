e9b7c5c519440a07dedffc0821bfeecee48d0e15
Jo Colina <jo@jocolina.com>
2015-06-07 14:14:08 +0200

    Solved most errors with code, have to solve interdependency Tache<->Projet, Tache should not have a member ParentProjet

--- ProjectCalendar/projet.h
+++ ProjectCalendar/projet.h
@@ -1,22 +1,40 @@
 #ifndef PROJET
 #define PROJET
 #include "ProjC.h"
+class Tache{};
 
-class Tache;
+/*!	\class Projet
+	\brief La classe qui definit les projets
 
+
+	Un projet a un nom, et des taches, il n'a pas de due date,
+	celle-ci ne depend que de la date d'echeance de la derniere
+	tache a effectuer. 
+*/
+	
 class Projet{
 	friend class Iterator;
 private:
-	QString nom;
-    std::vector<Tache*> taches;
+	QString nom; /*!< le Nom du projet, en QString*/
+    std::vector<Tache*> taches; /*!< Le vecteur des taches qui sont comprises dans le projet*/
 
 public:
+	//! Le constructeur Public du Projet
+	/*!
+		Le constructeur du Projet reserve 20 places pour les taches
+		\param n Nom du projet en QString
+	*/
     Projet(const QString& n): nom(n){
-		taches.reserve(10);
+		taches.reserve(20);
 	}
 
+	//!Le destructeur du Projet
+	/*!
+		Le destructeur du Projet qui elimine toutes les taches
+		qui lui sont associees
+	*/
 	~Projet(){
-		for (int i = 0; i < taches.size(); i++){
+        for (unsigned int i = 0; i < taches.size(); i++){
 			delete taches[i];
 		}
     }
@@ -24,13 +42,23 @@ public:
     /*********
      Accessors
     *********/
+    //! getTaches permet de recuperer tout le vecteur des taches
+    /*!
+		Methode dangereuse, etant donne qu'elle renvoit le vecteur complet
+    */
     const std::vector<Tache*> getTaches() const{ return taches; }
 
 
 	/**********
 	/ Setters
     *********/
-    void addTache(const Tache& tache){ taches.push_back(tache); }
+
+    //! Permet d'ajouter une tache au projet
+    /*!
+		addTache permet d'ajouter une tache au projet
+		\param tache en tant que reference a une tache
+    */
+    void addTache(Tache* tache){ taches.push_back(tache); }
 
     /*void addTaches(const vector<tache*> addingTaches){
 		for (int i = 0; i < addingTaches.size(); i++){
@@ -38,37 +66,44 @@ public:
 		}
 	}*/
 
-
-	//TODO:
-		// Creer iterateur
-    void deleteTache(const Tache& tache){
-		for (int i = 0; i < taches.size(); i++){
-            if (&taches[i] == tache){
+	//! Permet de detruire une tache du vecteur
+	/*!
+		Enelve une tache du projet tout en la laissant en memoire
+	*/
+    void deleteTache(Tache* tache){
+        for (unsigned int i = 0; i < taches.size(); i++){
+            if (taches[i] == tache){
 				taches.erase(taches.begin()+i);
 			}
 		}
 	}
 
 
-
+	/*! \class Iterator
+		\brief La classe iterator du vecteur de projet
+	
+		Cette classe permet d'iterer les taches du vecteur de projet.
+		En effet il est dangereux de laisser le vecteur au public, on
+		implemente alors un iterator qui rendra les taches une a une
+	*/
 	class Iterator{
 		friend class Projet;
 
 	private:
-		int i; //index
-		std::vector<Tache*> tac;
+        std::vector<Tache*> tac;
+        unsigned int i; //index
 
 		Iterator(std::vector<Tache*> taches):tac(taches), i(0){}
 	public:
 		~Iterator();
 
-        Tache& current(){
+        Tache* current(){
 			return tac[i];
 		}
 
 		void next(){ 
 			if (end()){
-				throw CalendarException("next dans iterator fini");
+//                throw new CalendarException("next dans iterator fini");
 			}else{
 				i++;
 			}
@@ -82,8 +117,9 @@ public:
 			}
 		}
 
-		static Iterator getIterator() const { return new Iterator(taches); }
 	};
+	
+	Iterator getIterator() const { return new Iterator(taches); }
 
 };
 #endif
--- ProjectCalendar/projetManager.h
+++ ProjectCalendar/projetManager.h
@@ -1,22 +1,79 @@
-#ifndef PROJETMANAGER_H
-#define PROJETMANAGER_H
+#ifndef PROJETMANAGER
+#define PROJETMANAGER
 #include "ProjC.h"
 
+/*	\class ProjetManager
+	\brief Cette classe sert a gerer les differents projets
+
+
+	Cette classe, un sigleton, permet au developpeur d'avoir 
+	une interface directe pour gerer les projets.
+	En effet, elle possede un vecteur de projets qui contiendra tous les projets 
+	du calendrier.
+*/
 class ProjetManager{
 
 friend class Iterator;
 private:
-	std::vector<Project*> projects;
-	ProjetManager();
+
+
+	std::vector<Project*> projects; /*!< Le vecteur qui contiendra les projets, en pointeur pour la memoire*/
+
+	//! Le constructeur, prive, du Project Manager
+	/*!
+		Le constructeur du projet permet de reserver un espace de 
+		10 projets dans le vecteur.
+	*/
+	ProjetManager(){
+		projects.reserve(10);
+	};
+
+	ProjetManager(const ProjetManager &old); // desactivation de la copie
+    const ProjetManager &operator=(const ProjetManager &old); // desactivation de "l'assignment"
+	
+	//!Destructeur de ProjectManager
+	/*!
+		Etant donne que le lien entre un projet et le projetManager
+		est une composition, le destructeur de ProjetManager doit
+		aussi detruire toutes les occurrences de projets dans
+		le vecteur qui leur est associe.
+		Le destructeur est prive, parce qu'il est dangereux de pouvoir 
+		eliminer le seul objet qui peut detruire la moitie des evts 
+		dans le cal
+	*/
+	~ProjetManager(){
+		for(unsigned int i = 0; i<projects.size(); i++){
+			delete projects[i];
+		}
+	};
 public:
-	~ProjetManager();
-	ProjetManager& getManager(){
-		return new ProjetManager();
+	
+	//!getManager, retourne l'instance du singleton ProjetManager
+	/*!
+		Cette methode cree, et retourne une instance unique de 
+		ProjetManager. Etant donne qu'elle est statique, elle 
+		peut etre appelee de tout code.
+	*/
+	static ProjetManager& getManager(){
+		static ProjetManager *instance = new ProjetManager;
+
+		return *instance;
 	}
 
+	//! Permet d'ajouter un projet au vecteur du ProjetManager
+	/*!
+		\param proj une reference au Projet qui sera ajoute
+	*/
 	void addProjet(Projet& proj){
-		projects.push_back(proj);
+		projects.push_back(*proj);
 	}
+
+	//! Permet d'eliminer un projet du ProjetManager
+	/*!
+		Elimine un projet mais le laisse en memoire, sinon utiliser
+		deleteProject
+		\param proj une reference au projet a eliminer
+	*/
 
 	void removeProject(Projet& proj){
 		for (int i = 0; i < projects.size(); i++){
@@ -26,7 +83,26 @@ public:
 		}
 	}
 
+	//! Permet de detruire un projet du ProjetManager
+	/*!
+		\param proj une reference au projet a detruire
+	*/
 
+	void deleteProject(Projet& proj){
+		for (int i = 0; i < projects.size(); i++){
+			if (&projects[i] == proj){
+				delete projects[i];
+			}
+		}
+	}
+
+	//! Iterator de ProjetManager
+	/*!
+		On cree un iterator pour ne pas laisser le vecteur libre
+		En effet il est dangereux de laisser la manipulation du vecteur 
+		au public, l'iterator nous permet de rendre projet par projet
+		et donc de ne pas publier tout le dataset.
+	*/
 	class Iterator{
 	
 		friend class ProjetManager;
